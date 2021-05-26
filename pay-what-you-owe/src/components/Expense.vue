964c4da69afea5819ce84378cab7a3c06f71cf29
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2021-05-26 09:38:52 +0100

    Income component added

--- pay-what-you-owe/src/components/Expense.vue
+++ pay-what-you-owe/src/components/Expense.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="container">
-      <h1>Nome Utilizador</h1>
+      <h1>Despesas</h1>
 
       <form @submit.prevent>
         <div class="mb-3">
@@ -10,83 +10,95 @@
           <v-select
             label="description"
             :options="types"
-            v-model="expense.type">
+            v-model="expense.type"
+            value="expense">
           </v-select>
         </div>
 
-        <div class="mb-3">
-          <label
-            for="description"
-            class="form-label">
-            
-            Descrição:</label>
+        <div v-if="expense.type">
+          <div class="mb-3">
+            <label
+              for="description"
+              class="form-label">
 
-          <input
-            id="description"
-            type="text"
-            class="form-control"
-            placeholder="Descrição"
-            v-model="expense.description">
-        </div>
+              Descrição:
+            </label>
 
-        <div class="mb-3">
-          <label
-            for="value"
-            class="form-label">
-            
-            Valor em dívida:
-          </label>
-
-          <div class="input-group mb-3">
             <input
-              id="value"
-              type="number"
+              id="description"
+              type="text"
               class="form-control"
-              placeholder="Valor"
-              v-model="expense.value">
-
-            <span class="input-group-text">€</span>
+              placeholder="Descrição"
+              v-model="expense.description">
           </div>
-        </div>
 
-        <div class="mb-3">
-          <label
-            for="date"
-            class="form-label">
-            
-            Data:
-          </label>
+          <div class="mb-3">
+            <label
+              for="value"
+              class="form-label">
 
-          <div class="input-group mb-3">
-            <input
-              id="date"
-              type="date"
-              class="form-control"
-              v-model="expense.date">
+              Valor em dívida:
+            </label>
+
+            <div class="input-group mb-3">
+              <input
+                id="value"
+                type="number"
+                class="form-control"
+                placeholder="Valor"
+                v-model="expense.value">
+
+              <span class="input-group-text">€</span>
+            </div>
           </div>
-        </div>
 
-        <button
-          @click="addExpensive()"
-          type="submit"
-          class="btn btn-primary">
-          
-          Adicionar Despesa
-        </button>
+          <div class="mb-3">
+            <label
+              for="date"
+              class="form-label">
+
+              Data:
+            </label>
+
+            <div class="input-group mb-3">
+              <input
+                id="date"
+                type="date"
+                class="form-control"
+                v-model="expense.date">
+            </div>
+          </div>
+
+          <button
+            @click="addExpensive()"
+            type="submit"
+            class="btn btn-primary">
+
+            Adicionar Despesa
+          </button>
+        </div>
       </form>
-      
+
       <div class="mt-3"
         v-for="expense in expenses"
         :key="expense.id">
 
         {{ expense.type }}: {{ expense.value + '€'}}
       </div>
+
+      <income />
     </div>
   </div>
 </template>
 
 <script>
+import Income from '@/components/Income.vue'
+
 export default {
+  components: {
+    Income
+  },
+
   data () {
     return {
       expense: {
@@ -137,7 +149,7 @@ export default {
         date: this.expense.date
       }
 
-      this.expenses.push(newExpense)      
+      this.expenses.push(newExpense)
 
       this.clearForm()
     },
---
+++ pay-what-you-owe/src/components/Income.vue
@@ -0,0 +1,150 @@
+<template>
+  <div>
+    <div class="container">
+      <h1>Proveitos</h1>
+
+      <form @submit.prevent>
+        <div class="mb-3">
+          <label class="form-label">Tipo de proveito:</label>
+
+          <v-select
+            label="description"
+            :options="types"
+            v-model="income.type">
+          </v-select>
+        </div>
+
+        <div class="mb-3">
+          <label
+            for="description"
+            class="form-label">
+            
+            Descrição:</label>
+
+          <input
+            id="description"
+            type="text"
+            class="form-control"
+            placeholder="Descrição"
+            v-model="income.description">
+        </div>
+
+        <div class="mb-3">
+          <label
+            for="value"
+            class="form-label">
+            
+            Valor adicionado:
+          </label>
+
+          <div class="input-group mb-3">
+            <input
+              id="value"
+              type="number"
+              class="form-control"
+              placeholder="Valor"
+              v-model="income.value">
+
+            <span class="input-group-text">€</span>
+          </div>
+        </div>
+
+        <div class="mb-3">
+          <label
+            for="date"
+            class="form-label">
+            
+            Data:
+          </label>
+
+          <div class="input-group mb-3">
+            <input
+              id="date"
+              type="date"
+              class="form-control"
+              v-model="income.date">
+          </div>
+        </div>
+
+        <button
+          @click="addIncome()"
+          type="submit"
+          class="btn btn-primary">
+          
+          Adicionar proveito
+        </button>
+      </form>
+      
+      <div class="mt-3"
+        v-for="income in incomes"
+        :key="income.id">
+
+        {{ income.type }}: {{ income.value + '€'}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      income: {
+        type: null,
+        description: '',
+        value: '',
+        date: ''
+      },
+
+      incomes: [],
+
+      types: [
+        {
+          id: 1,
+          description: 'Salário'
+        },
+
+        {
+          id: 2,
+          description: 'Prémio'
+        },
+
+        {
+          id: 3,
+          description: 'Presentes'
+        },
+
+        {
+          id: 4,
+          description: 'Outro'
+        }
+      ]
+    }
+  },
+
+  methods: {
+    addIncome () {
+      let newIncome = {
+        id: this.income.length + 1,
+        type: this.income.type.description,
+        description: this.income.description,
+        value: this.income.value,
+        date: this.income.date
+      }
+
+      this.incomes.push(newIncome)
+
+      this.clearIncomeForm()
+    },
+
+    clearIncomeForm () {
+      this.income = {
+        type: null,
+        description: '',
+        value: '',
+        date: ''
+      }
+    }
+  }
+}
+</script>
